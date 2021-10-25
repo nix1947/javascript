126402a80fcac9ab693f25041c6ffe13f8566aed
Manoj Gautam <nix1947@users.noreply.github.com>
2021-10-25 06:02:56 +0545

    Update js.js

--- js.js
+++ js.js
@@ -11,3 +11,23 @@
 8. bigInt
 
 */
+
+
+/* Postgres 
+
+-- Check for UUID plugin 
+> SELECT * FROM pg_available_extensions; 
+
+-- Installing uuid-ossp plugin 
+> create extension uuid-ossp if not exists; 
+
+-- Generate uuid for column, uuid_generate_v1() function is available
+> uuid_generate_v1()
+
+
+*/
+
+
+/* Excel insert query
+insert into tablename(title, description, created_date) values(' "&A1&" ', ' "&A2" ', '2010-5-12')
+*/
